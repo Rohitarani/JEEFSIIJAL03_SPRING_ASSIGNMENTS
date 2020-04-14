57c595fd980847d76236db1b909b79564e8083aa
TheDictator <TheDictator@LAPTOP-2366GP4L>
2020-04-14 18:51:41 +0530

    1

---
+++ SpringBootAssignment5Task3/src/main/java/com/cg/SpringBootAssignment5Task3Application.java
@@ -0,0 +1,18 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+
+
+public class SpringBootAssignment5Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignment5Task3Application.class, args);
+	}
+
+	
+
+}
