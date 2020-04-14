89ca72d747ce479476e0479ea24d67fee260ef87
TheDictator <TheDictator@LAPTOP-2366GP4L>
2020-04-14 18:52:34 +0530

    1

---
+++ SpringBootAssignment5Task4/src/main/java/com/cg/SpringBootAssignment5Task4Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignment5Task4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignment5Task4Application.class, args);
+	}
+
+}
